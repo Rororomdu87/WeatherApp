b8084dff13915dc62fb68f369ceed018f3b00635
Rororomdu87 <rororom87@gmail.com>
2017-03-21 18:56:16 +0100

    Détails villes

--- app/src/main/java/com/example/romain/weatherapp/ActivityInfosVilles.java
+++ app/src/main/java/com/example/romain/weatherapp/ActivityInfosVilles.java
@@ -9,6 +9,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class ActivityInfosVilles extends AppCompatActivity {
 
     private static String API_KEY = "e6fb900e3feb77b61e5b8fbe065f8b1d";
@@ -41,5 +48,28 @@ public class ActivityInfosVilles extends AppCompatActivity {
         paysVille.setText("Pays : " + city.getCountry());
         latVille.setText("Latitude : " + city.getCoord().getLat());
         lonVille.setText("Longitude : " + city.getCoord().getLon());
+
+        final TextView mTextView = (TextView) findViewById(R.id.text);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://api.openweathermap.org/data/2.5/weather?id=" + city.get_id() + "&appid=" + API_KEY;
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
